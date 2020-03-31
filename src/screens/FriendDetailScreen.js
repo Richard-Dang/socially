33e21f3fbfcb44ba907169d8f5fdeeee3f6fa587
Richard Dang <richardspacedang@gmail.com>
2020-03-31 00:31:26 -0400

    Add screens

---
+++ src/screens/FriendDetailScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const FriendDetailScreen = () => {
+  return (
+    <View>
+      <Text>FriendDetailScreen</Text>
+    </View>
+  );
+};
+
+export default FriendDetailScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/FriendListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const FriendListScreen = () => {
+  return (
+    <View>
+      <Text>FriendListScreen</Text>
+    </View>
+  )
+}
+
+export default FriendListScreen
+
+const styles = StyleSheet.create({})
